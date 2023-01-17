043f390ccae5b60244e30d1d6a3bb39db7e50744
Nice Gnome <nicegnome1@gmail.com>
2023-01-16 16:14:14 -0800

    set 1 challenge 1

--- set-1.py
+++ set-1.py
@@ -2,5 +2,20 @@
 # 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 # Should produce:
 # SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-# Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+# 
+from base64 import b64encode
+
+# hex -> base64
+# s = 'cafebabe'
+# b64 = b64encode(bytes.fromhex(s)).decode()
+# print('cafebabe in base64:', b64)
+
+def hex_to_base64(hex):
+    return str(b64encode(bytes.fromhex(hex)).decode())
+
+# test
+print(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')=='SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
+
+
+
 
