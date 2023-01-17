71540f54b80814666162ca637357517f01f7aef4
Nice Gnome <nicegnome1@gmail.com>
2023-01-16 16:01:54 -0800

    set-1 test commit

---
+++ set-1.py
@@ -0,0 +1,6 @@
+# Convert hex to base64
+# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# Should produce:
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+# Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+
